f2daa6468631b49a8f3f56361d289c1827d3e95d
ONTHEFLUX-coder <linoche6900@gmail.com>
2026-06-25 15:42:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.voice_states = True 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
